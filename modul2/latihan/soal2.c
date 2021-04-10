7c660cdc44a3533d3898b993f43efb88a117666b
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2021-04-10 20:08:22 +0700

    Update soal2.c

--- modul2/latihan/soal2.c
+++ modul2/latihan/soal2.c
@@ -10,8 +10,8 @@
 #include <time.h>
 
 int main() {
-	  time_t rawtime;
-  	struct tm* timeinfo;
+    time_t rawtime;
+    struct tm* timeinfo;
     char buffer[80];
     time(&rawtime);
 	  timeinfo = localtime(&rawtime);
