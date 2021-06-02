2e856449ce43285cdbdad1150514d1d03028ed59
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2021-06-02 14:22:52 +0700

    Create jagofs.c

---
+++ modul4/latihan/jagofs.c
@@ -0,0 +1,167 @@
+// note: scuffed code
+
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static  const  char *dirpath = "/home/xa/Downloads";
+
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+char *multi_tok(char *input, char *delimiter) {
+    static char *string;
+    if (input != NULL)
+        string = input;
+
+    if (string == NULL)
+        return string;
+
+    char *end = strstr(string, delimiter);
+    if (end == NULL) {
+        char *temp = string;
+        string = NULL;
+        return temp;
+    }
+
+    char *temp = string;
+
+    *end = '\0';
+    string = end + strlen(delimiter);
+    return temp;
+}
+
+static  int  xmp_getattr(const char *path, struct stat *stbuf)
+{
+    int res;
+    char fpath[1000];
+
+    sprintf(fpath,"%s%s",dirpath,path);
+
+    res = lstat(fpath, stbuf);
+
+    if (res == -1) return -errno;
+
+    return 0;
+}
+
+
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+    char fpath[1000];
+
+    if(strcmp(path,"/") == 0)
+    {
+        path=dirpath;
+        sprintf(fpath,"%s",path);
+    } else sprintf(fpath, "%s%s",dirpath,path);
+
+    int res = 0;
+
+    DIR *dp;
+    struct dirent *de;
+    (void) offset;
+    (void) fi;
+
+    dp = opendir(fpath);
+
+    if (dp == NULL) return -errno;
+
+    while ((de = readdir(dp)) != NULL) {
+        struct stat st;
+
+        memset(&st, 0, sizeof(st));
+
+        st.st_ino = de->d_ino;
+        st.st_mode = de->d_type << 12;
+        char* jago;
+        if(strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0){
+            jago = de->d_name;
+        } else {
+            jago = concat("Jago_", de->d_name);
+        }
+        res = (filler(buf, jago, &st, 0));
+
+        if(res!=0) break;
+    }
+
+    closedir(dp);
+
+    return 0;
+}
+
+
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+    char fpath[1000];
+    if(strcmp(path,"/") == 0)
+    {
+        path=dirpath;
+
+        sprintf(fpath,"%s",path);
+    }
+    else sprintf(fpath, "%s%s",dirpath,path);
+
+    int res = 0;
+    int fd = 0 ;
+
+    (void) fi;
+
+    // char * token = strtok(fpath, "Jago_");
+    char * token = multi_tok(fpath, "Jago_");
+    char * new_fpath = fpath;
+    while( token != NULL ) {
+        // printf( " %s\n", token ); //printing each token
+        new_fpath = concat(new_fpath, token);
+        // sprintf(new_fpath, "%s%s",input_new_fpath,token);
+        token = multi_tok(NULL, "Jago_");
+    }
+
+    fd = open(new_fpath, O_RDONLY);
+
+    if (fd == -1) return -errno;
+
+    res = pread(fd, buf, size, offset);
+
+    if (res == -1) res = -errno;
+
+    close(fd);
+
+    return res;
+}
+
+
+
+static struct fuse_operations xmp_oper = {
+    .getattr = xmp_getattr,
+    .readdir = xmp_readdir,
+    .read = xmp_read,
+};
+
+
+
+int  main(int  argc, char *argv[])
+{
+    umask(0);
+
+    return fuse_main(argc, argv, &xmp_oper, NULL);
+}
+
+// gcc -Wall `pkg-config fuse --cflags` jagofs_tes.c -o jagofs_tes `pkg-config fuse --libs`
+// ./jagofs_tes JagoFS
+// fusermount -u JagoFS
